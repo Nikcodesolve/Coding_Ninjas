1bd8d8d72ee46c07ff2dab979df7be85e27d4a79
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:47:54 +0530

    Update fibonacci member.py

--- Functions/fibonacci member.py
+++ Functions/fibonacci member.py
@@ -1,1 +1,20 @@
 
+def fib(n):
+    if n==0 or n==1 or n==2:
+       return True
+    f1=0
+    f2=1
+    for i in range(n):
+        f3=f1+f2
+        f1=f2
+        f2=f3
+        if n==f2:
+            return True
+        return False
+        
+
+n=int(input())
+if fib(n):
+    print("true")
+else:
+        print("false")
