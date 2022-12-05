8f98001b21fe7ea2d6d377d4a085eef8440aa733
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:46:26 +0530

    Update diamond of stars.py

--- more on loops/diamond of stars.py
+++ more on loops/diamond of stars.py
@@ -1,1 +1,10 @@
-
+n=int(input())
+for i in range((n+1)//2):
+    print(" "*(((n+1)//2)-(i+1)),end="")
+    print("*"*(2*i+1),end="")
+    print()
+    
+for k in range(1, ((n-1)//2)+1):
+    print(" "*k, end="")
+    print("*"*(n-2*k), end="")
+    print()
