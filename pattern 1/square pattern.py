3ee7c3808a8c0515cfe77db6b86f2fe2546fe8b9
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:38:11 +0530

    Update square pattern.py

--- pattern 1/square pattern.py
+++ pattern 1/square pattern.py
@@ -1,1 +1,13 @@
 
+## Read input as specified in the question
+## Print the required output in given format
+N=int(input())
+i=1
+while i<=N:
+    j=1
+    while j<=N:
+        print(N,end="")
+        
+        j=j+1
+    print()  
+    i=i+1
