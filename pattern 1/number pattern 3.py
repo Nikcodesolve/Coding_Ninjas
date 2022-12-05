4e818f32edebaa6ead8655cd43a9989aa61b977c
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:40:46 +0530

    Update number pattern 3.py

--- pattern 1/number pattern 3.py
+++ pattern 1/number pattern 3.py
@@ -1,1 +1,15 @@
 
+n = int(input())
+i = 1
+while i <= n:
+    j = 1
+    while j <= i:
+        if j == 1:
+            print(j, end='')
+        elif j == i:
+            print('1', end='')
+        else:
+            print('2', end='')
+        j = j + 1
+    print()
+    i = i + 1
