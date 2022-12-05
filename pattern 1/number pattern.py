99bb96ad4885d18b09703dea2f7ddfa5b563efe3
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:41:26 +0530

    Update number pattern.py

--- pattern 1/number pattern.py
+++ pattern 1/number pattern.py
@@ -1,1 +1,13 @@
 
+# N=int(input())
+# for i in range(N):
+#    # print(1,end="")
+#     for j in range(1, (N+1) - i): 
+#         print(j,end="")
+#     print()
+n=int(input())
+for i in range(1,n+1):
+    print("1",end="")
+    for j in range(1,(n-i)+1):
+        print(j+1,end="")
+    print()
