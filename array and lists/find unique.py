295b9121b85dc15611e3e1956a9e3f38a1a429a6
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:49:50 +0530

    Update find unique.py

--- array and lists/find unique.py
+++ array and lists/find unique.py
@@ -1,1 +1,53 @@
 
+import sys
+
+def findUnique(arr, n) :
+    #Your code goes here
+    for i in arr:
+        count = 0
+        for j in arr:
+            if i == j:
+                count += 1
+        if count == 1:
+            return i
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Taking Input Using Fast I/O
+def takeInput() :
+    n = int(sys.stdin.readline().rstrip())
+
+    if n == 0 :
+        return list(), 0
+
+    arr = list(map(int, sys.stdin.readline().rstrip().split(" ")))
+    return arr, n
+
+
+#main
+t = int(sys.stdin.readline().rstrip())
+
+while t > 0 :
+
+    arr, n = takeInput()
+    print(findUnique(arr, n))
+
+    t -= 1
