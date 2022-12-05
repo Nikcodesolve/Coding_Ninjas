898b2da144ddd08b2b37494e34b9252f0c9b9f1d
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:39:15 +0530

    Update reverse number  pattern.py

--- pattern 1/reverse number  pattern.py
+++ pattern 1/reverse number  pattern.py
@@ -1,1 +1,12 @@
 
+## Read input as specified in the question
+## Print the required output in given format
+n=int(input())
+i=1
+while i<=n:
+    j=1
+    while j<=i:
+        print(i-j+1,end="")
+        j+=1
+    print()    
+    i+=1
