fbc2edd252f3c431e5ad182a937c43780392520c
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:39:35 +0530

    Update character pattern.py

--- pattern 1/character pattern.py
+++ pattern 1/character pattern.py
@@ -1,1 +1,12 @@
 
+## Read input as specified in the question
+## Print the required output in given format
+N=int(input())
+i=1
+while i<=N:
+    j=1
+    while j<=i:
+        print(chr(65+i+j-2),end="")
+        j+=1
+    print()
+    i+=1 
