75d8b03488f899c95c48348eb6088062ca31859c
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:44:44 +0530

    Update pyramid number pattern.py

--- pattern 2/pyramid number pattern.py
+++ pattern 2/pyramid number pattern.py
@@ -1,1 +1,32 @@
+n = int(input())
 
+# Row Count
+i = 1
+while i <= n:
+
+    # Print Spaces
+    spaces = n - i
+    for _ in range(spaces):
+        print(" ", end='')
+
+    # part 1 of pattern (Left Side)
+    j1 = 2
+    count1 = i
+    while j1 <= (i):
+        print(count1, end="")
+        count1 = count1 - 1
+        j1 = j1 + 1
+
+    print("1", end="")
+
+    # Part 2 of the pattern (Right side)
+    j2 = 2
+    while j2 <= (i):
+        print(j2, end="")
+        j2 = j2 + 1
+
+    # Print New line
+    print()
+
+    # Add New row
+    i = i + 1
