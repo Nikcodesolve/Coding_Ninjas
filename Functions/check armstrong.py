8031c615b3bca58ba830136e1273ed1040772abd
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:48:36 +0530

    Update check armstrong.py

--- Functions/check armstrong.py
+++ Functions/check armstrong.py
@@ -1,1 +1,20 @@
+def check_armstrong(n):
+    arm_num = 0
+    dummy = n
+    dgts = len(str(n))
+    for i in range(0, dgts, 1):
+        last_dgt = dummy % 10
+        arm_num = arm_num + (last_dgt ** int(dgts))
+        dummy = dummy // 10
 
+    if arm_num == n:
+        return True
+    else:
+        return False
+
+
+n = int(input())
+if check_armstrong(n):
+    print("true")
+else:
+    print("false")
