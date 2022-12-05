bbd6006a0af9f1cafd3eadf33493421aecf0d0fd
Nikcodesolve <119833889+Nikcodesolve@users.noreply.github.com>
2022-12-05 12:52:20 +0530

    Update array sum.py

--- array and lists/array sum.py
+++ array and lists/array sum.py
@@ -1,1 +1,9 @@
+## Read input as specified in the question.
+## Print output as specified in the question.
+n = int(input())
+li = list(input().split( ))
+sum1 = 0
+for i in li:
+    sum1 = sum1 + int(i)
 
+print(sum1)
